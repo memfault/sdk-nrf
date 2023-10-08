6add14d56bc99d3b83f5afcd740119a4c6f1a329
Chris Coleman <chris@memfault.com>
2023-10-08 16:02:29 -0400

    tfm: NCSDK-24156 Handle NS busfaults with Zephyr fault infrastructure
      ### Background Context
    
    In NCS v2.1.0, Secure Partition Manager (SPM) was deprecated in favor
    of TFM for the trusted execution environment on the nRF53/91
    series. This cutover resulted in a change in behavior causing all
    BusFaults to get forwarded to the TFM fault handlers _even_ when they
    are caused by non-secure firmware and Zephyrs fault handler never gets
    called.
    
    This is problematic for several reasons:
    
     - Slows down local debug because fault information no longer gets
       printed in Zephyr shell.
     - Customers debugging failures remotely in the field (using tools
       like Memfault [^1]) no longer get information about any busfaults
       (caused by common programmatic errors like a null pointer
       dereference).
    
    [^1]: https://developer.nordicsemi.com/nRF_Connect_SDK/doc/latest/nrf/libraries/debug/memfault_ncs.html
    
    This PR restores SPM-like functionality by calling the Zephyr fault
    handler when a busfault is triggered by non-secure code.
    
      ### Summary of Changes
    
     - enabled capturing of exception info when TFM fault handlers are
       invoked (required `-DTFM_EXCEPTION_INFO_DUMP=True`)
     - Added a small change[^2] to sdk-trusted-firmware-m to expose exception info via API
     - Implemented custom platform reset handlers
       PLATFORM_DEFAULT_SYSTEM_RESET_HALT (instead of using the default
       ones from [platform/ext/common/tfm_hal_reset_halt.c [^3]
     - Added Kconfig opt, `CONFIG_TFM_ALLOW_NON_SECURE_FAULT_HANDLING=y`,
       to selectively turn on/off functionality
    
    The function of most interest to review is `tfm_hal_system_reset()`
    which takes advantage of a few properities related to ARMv8-M
    exception handling (see the Armv8-M Architecture Reference Manual
    [^4]) for more details. Notably,
    
     - BusFault's are not banked between NS/S and by default will generate
       a secure exception.
     - Any attempt to access secure memory will trigger a SecureFault
     - When a Secure exception is executed we can use the info in
       EXC_RETURN to determine whether the fault originated from the
       Secure or Non-Secure side.
    
    Piecing these properties together -- from the SecureFault and BusFault
    handlers we check to see if the fault originated from the non-secure
    side and iff that is the case, invoke the non-secure Zephyr fault
    handler to save off exception info. Note these handlers are at
    configurable interrupt priority and AIRCR.PRIS is set -- so any error
    from the Zephyr fault handler will trap back into the secure HardFault
    handler and the device will just be reset.
    
    [^2]: https://github.com/nrfconnect/sdk-trusted-firmware-m/compare/master...memfault:sdk-trusted-firmware-m:ccoleman/exc_info?expand=1
    [^3]: https://github.com/nrfconnect/sdk-trusted-firmware-m/blob/master/platform/ext/common/tfm_hal_reset_halt.c
    [^4]: https://developer.arm.com/documentation/ddi0553/bw/?lang=en
    
      ### Test Plan
    
     - Tested against v2.4 with the above patch applied to sdk-nrf and
       trusted-firmware-m
     - Instrument code to trigger a null pointer dereference and observe
       that the Zephyr fault handler gets invoked
    
    ```
      volatile uint32_t *null_ptr =(void *)0x0;
      *null_ptr = 0xbadcafe;
    ```
     - Flashed `nrf/samples/debug/memfault` sample and confirmed fault
       paths which previously did not get captured, now invoke the Zephyr
       handler:
       - `mflt test busfault`
       - `mflt test double_free`
    
    Signed-off-by: Chris Coleman <chris@memfault.com>

--- modules/tfm/tfm/boards/common/tfm_hal_platform.c
+++ modules/tfm/tfm/boards/common/tfm_hal_platform.c
@@ -21,6 +21,8 @@
 #include "tfm_spm_log.h"
 #include "hw_unique_key.h"
 #include "config_tfm.h"
+#include "exception_info.h"
+#include "tfm_arch.h"
 
 #if defined(TFM_PARTITION_CRYPTO)
 static enum tfm_hal_status_t crypto_platform_init(void)
@@ -115,3 +117,117 @@ enum tfm_hal_status_t tfm_hal_platform_init(void)
 
 	return TFM_HAL_SUCCESS;
 }
+
+#if defined(CONFIG_TFM_ALLOW_NON_SECURE_FAULT_HANDLING)
+void tfm_hal_system_halt(void)
+{
+	/*
+	 * Disable IRQs to stop all threads, not just the thread that
+	 * halted the system.
+	 */
+	__disable_irq();
+
+	/*
+	 * Enter sleep to reduce power consumption and do it in a loop in
+	 * case a signal wakes up the CPU.
+	 */
+	while (1) {
+		__WFE();
+	}
+}
+
+#define SECUREFAULT_EXCEPTION_NUMBER 7
+#define HARDFAULT_EXCEPTION_NUMBER   3
+#define BUSFAULT_EXCEPTION_NUMBER    5
+
+#if defined(TFM_EXCEPTION_INFO_DUMP) && defined(TRUSTZONE_PRESENT)
+
+__attribute__((naked)) static void handle_fault_from_ns(
+	uint32_t fault_handler_fn, uint32_t exc_return) {
+	/* scrub secure register state before jumping to ns */
+	__ASM volatile(
+		"mov  lr, r1 \n"
+		"movs r1, #0 \n"
+		"movs r2, #0 \n"
+#if (CONFIG_TFM_FLOAT_ABI >= 1)
+		"vmov d0, r1, r2 \n"
+		"vmov d1, r1, r2 \n"
+		"vmov d2, r1, r2 \n"
+		"vmov d3, r1, r2 \n"
+		"vmov d4, r1, r2 \n"
+		"vmov d5, r1, r2 \n"
+		"vmov d6, r1, r2 \n"
+		"vmov d7, r1, r2 \n"
+		"mrs r2, control \n"
+		"bic r2, r2, #4 \n"
+		"msr control, r2 \n"
+		"isb \n"
+#endif
+		"ldr  r1, ="M2S(STACK_SEAL_PATTERN)" \n"
+		"push {r1, r2} \n"
+		"movs r1, #0 \n"
+		"movs r3, #0 \n"
+		"movs r4, #0 \n"
+		"movs r5, #0 \n"
+		"movs r6, #0 \n"
+		"movs r7, #0 \n"
+		"movs r8, #0 \n"
+		"movs r9, #0 \n"
+		"movs r10, #0 \n"
+		"movs r11, #0 \n"
+		"movs r12, #0 \n"
+		"bic r0, r0, #1 \n"
+		"bxns r0 \n"
+	);
+}
+
+void tfm_hal_system_reset(void)
+{
+	struct exception_info_t *exc_ctx = tfm_exception_info_get_context();
+	const uint8_t active_exception_number = (exc_ctx->xPSR & 0xff);
+	const bool securefault_active = (active_exception_number == SECUREFAULT_EXCEPTION_NUMBER);
+	const bool busfault_active = (active_exception_number == BUSFAULT_EXCEPTION_NUMBER);
+
+	if ((exc_ctx == NULL) || is_return_secure_stack(exc_ctx->EXC_RETURN) ||
+		!(securefault_active || busfault_active)) {
+		NVIC_SystemReset();
+	}
+
+	/*
+	 * If we get here, we are taking a reset path where a fault was generated
+	 * from the NS firmware running on the device. If we just reset, it will be
+	 * impossible to extract the root cause of the error on the NS side.
+	 *
+	 * To allow for root cause analysis, let's call the NS HardFault handler.  Any error from
+	 * the NS fault handler will land us back in the Secure HardFault handler where we will not
+	 * enter this path and simply reset the device.
+	 */
+
+	uint32_t *vtor = (uint32_t *)tfm_hal_get_ns_VTOR();
+
+	uint32_t hardfault_handler_fn = vtor[HARDFAULT_EXCEPTION_NUMBER];
+
+	/* bit 0 needs to be cleared to transition to NS */
+	hardfault_handler_fn &= ~0x1;
+
+	/* Adjust EXC_RETURN value to emulate NS exception entry */
+	uint32_t ns_exc_return = exc_ctx->EXC_RETURN & ~EXC_RETURN_EXC_SECURE;
+	/* Update SPSEL to reflect correct CONTROL_NS.SPSEL setting */
+	ns_exc_return &= ~(EXC_RETURN_SPSEL);
+	CONTROL_Type ctrl_ns;
+	ctrl_ns.w = __TZ_get_CONTROL_NS();
+	if (ctrl_ns.b.SPSEL) {
+		ns_exc_return |= EXC_RETURN_SPSEL;
+	}
+
+	handle_fault_from_ns(hardfault_handler_fn, ns_exc_return);
+
+	NVIC_SystemReset();
+}
+#else
+void tfm_hal_system_reset(void)
+{
+	NVIC_SystemReset();
+}
+#endif /* defined(TRUSTZONE_PRESENT) && defined(TFM_EXCEPTION_INFO_DUMP) */
+#endif /* CONFIG_TFM_ALLOW_NON_SECURE_FAULT_HANDLING */
