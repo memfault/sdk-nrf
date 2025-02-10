7d22652234d5386da787377722d36301f43571f6
Noah Pendleton <2538614+noahp@users.noreply.github.com>
2025-02-10 13:19:30 -0500

    ecdsa cert test
    from root of ncs workspace (memfault project key doesn't really matter
    though, since we only care about connecting to the test domain):
    
    ```bash
    ❯ west build -b nrf9160dk_nrf9160_ns@1.0.0 --pristine=always nrf/samples/debug/memfault -- -DCONFIG_MEMFAULT_NCS_PROJECT_KEY=\"$(<~/.memfault-noah-test-project-key)\" -Dmcuboot_CONFIG_BUILD_OUTPUT_META=n -DCONFIG_BUILD_OUTPUT_META=n -DCONFIG_NET_SHELL=y -DCONFIG_AT_SHELL=y && west flash
    ```

--- samples/debug/memfault/src/main.c
+++ samples/debug/memfault/src/main.c
@@ -138,9 +138,243 @@ static void connectivity_event_handler(struct net_mgmt_event_callback *cb, uint3
 	}
 }
 
+static const char cert[] = {
+	// // memfault g2 cert
+	// "-----BEGIN CERTIFICATE-----\n"
+	// "MIIDjjCCAnagAwIBAgIQAzrx5qcRqaC7KGSxHQn65TANBgkqhkiG9w0BAQsFADBh\n"
+	// "MQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3\n"
+	// "d3cuZGlnaWNlcnQuY29tMSAwHgYDVQQDExdEaWdpQ2VydCBHbG9iYWwgUm9vdCBH\n"
+	// "MjAeFw0xMzA4MDExMjAwMDBaFw0zODAxMTUxMjAwMDBaMGExCzAJBgNVBAYTAlVT\n"
+	// "MRUwEwYDVQQKEwxEaWdpQ2VydCBJbmMxGTAXBgNVBAsTEHd3dy5kaWdpY2VydC5j\n"
+	// "b20xIDAeBgNVBAMTF0RpZ2lDZXJ0IEdsb2JhbCBSb290IEcyMIIBIjANBgkqhkiG\n"
+	// "9w0BAQEFAAOCAQ8AMIIBCgKCAQEAuzfNNNx7a8myaJCtSnX/RrohCgiN9RlUyfuI\n"
+	// "2/Ou8jqJkTx65qsGGmvPrC3oXgkkRLpimn7Wo6h+4FR1IAWsULecYxpsMNzaHxmx\n"
+	// "1x7e/dfgy5SDN67sH0NO3Xss0r0upS/kqbitOtSZpLYl6ZtrAGCSYP9PIUkY92eQ\n"
+	// "q2EGnI/yuum06ZIya7XzV+hdG82MHauVBJVJ8zUtluNJbd134/tJS7SsVQepj5Wz\n"
+	// "tCO7TG1F8PapspUwtP1MVYwnSlcUfIKdzXOS0xZKBgyMUNGPHgm+F6HmIcr9g+UQ\n"
+	// "vIOlCsRnKPZzFBQ9RnbDhxSJITRNrw9FDKZJobq7nMWxM4MphQIDAQABo0IwQDAP\n"
+	// "BgNVHRMBAf8EBTADAQH/MA4GA1UdDwEB/wQEAwIBhjAdBgNVHQ4EFgQUTiJUIBiV\n"
+	// "5uNu5g/6+rkS7QYXjzkwDQYJKoZIhvcNAQELBQADggEBAGBnKJRvDkhj6zHd6mcY\n"
+	// "1Yl9PMWLSn/pvtsrF9+wX3N3KjITOYFnQoQj8kVnNeyIv/iPsGEMNKSuIEyExtv4\n"
+	// "NeF22d+mQrvHRAiGfzZ0JFrabA0UWTW98kndth/Jsw1HKj2ZL7tcu7XUIOGZX1NG\n"
+	// "Fdtom/DzMNU+MeKNhJ7jitralj41E6Vf8PlwUHBHQRFXGU7Aj64GxJUTFy8bJZ91\n"
+	// "8rGOmaFvE7FBcf6IKshPECBV1/MUReXgRPTqh5Uykw7+U0b6LJ3/iyK5S9kJRaTe\n"
+	// "pLiaWN0bfVKfjllDiIGknibVb63dDcY3fe0Dkhvld1927jyNxF1WW6LZZm6zNTfl\n"
+	// "MrY=\n"
+	// "-----END CERTIFICATE-----\n"
+
+	// memfault self-signed ecdsa root
+	"-----BEGIN CERTIFICATE-----\n"
+	"MIICczCCAfigAwIBAgIUKbNJGb1rWF1kSBv+dNIXnFHwQY0wCgYIKoZIzj0EAwMw\n"
+	"cDELMAkGA1UEBhMCVVMxEzARBgNVBAgMCkNhbGlmb3JuaWExFjAUBgNVBAcMDVNh\n"
+	"biBGcmFuY2lzY28xFzAVBgNVBAoMDk1lbWZhdWx0LCBJbmMuMRswGQYDVQQDDBJF\n"
+	"Q0RTQSBUZXN0IFJvb3QgQ0EwHhcNMjUwMjA1MTQ0MTA3WhcNMzUwMjAzMTQ0MTA3\n"
+	"WjBwMQswCQYDVQQGEwJVUzETMBEGA1UECAwKQ2FsaWZvcm5pYTEWMBQGA1UEBwwN\n"
+	"U2FuIEZyYW5jaXNjbzEXMBUGA1UECgwOTWVtZmF1bHQsIEluYy4xGzAZBgNVBAMM\n"
+	"EkVDRFNBIFRlc3QgUm9vdCBDQTB2MBAGByqGSM49AgEGBSuBBAAiA2IABLHDgHoq\n"
+	"vEhPNE+P4gs3j/GsBsJ6n/uXIN+4fQmNvBiHO9pRhrYJHNsnek8mL4LoHRSkvT7t\n"
+	"cG1+88+oSLJL4CrmSw1UzXxGUM4GFBCbYic6CBN+DhWeSyzYa5FRdCsxOqNTMFEw\n"
+	"HQYDVR0OBBYEFGjzgnfHwMRt9+h8m5Kl7soXiWBhMB8GA1UdIwQYMBaAFGjzgnfH\n"
+	"wMRt9+h8m5Kl7soXiWBhMA8GA1UdEwEB/wQFMAMBAf8wCgYIKoZIzj0EAwMDaQAw\n"
+	"ZgIxAIeJhxa6SAbIMgUyxVJfbRJoSEsTabdRu8UKpaZEdCNFH+smUIf4L/9up6Pn\n"
+	"7A+IjQIxAMRnSurxzIkr5LQNk/FWilw+WkUdUeU9spjocCTqP6nG2MTYOqxks4a1\n"
+	"1QtV37cwuw==\n"
+	"-----END CERTIFICATE-----\n"
+
+	/* Null terminate certificate if running Mbed TLS on the application core.
+	 * Required by TLS credentials API.
+	 */
+	IF_ENABLED(CONFIG_TLS_CREDENTIALS, (0x00))
+};
+
+BUILD_ASSERT(sizeof(cert) < KB(4), "Certificate too large");
+
+#include <modem/modem_key_mgmt.h>
+
+#define TLS_SEC_TAG		42
+
+/* Provision certificate to modem */
+int cert_provision(void)
+{
+	int err;
+
+	LOG_INF("Provisioning certificate");
+
+#if CONFIG_MODEM_KEY_MGMT
+	bool exists;
+	int mismatch;
+
+	/* It may be sufficient for you application to check whether the correct
+	 * certificate is provisioned with a given tag directly using modem_key_mgmt_cmp().
+	 * Here, for the sake of the completeness, we check that a certificate exists
+	 * before comparing it with what we expect it to be.
+	 */
+	err = modem_key_mgmt_exists(TLS_SEC_TAG, MODEM_KEY_MGMT_CRED_TYPE_CA_CHAIN, &exists);
+	if (err) {
+		LOG_ERR("Failed to check for certificates err %d", err);
+		return err;
+	}
+
+	if (exists) {
+		mismatch = modem_key_mgmt_cmp(TLS_SEC_TAG, MODEM_KEY_MGMT_CRED_TYPE_CA_CHAIN, cert,
+					      sizeof(cert) - 1);
+		if (!mismatch) {
+			LOG_INF("Certificate match");
+			return 0;
+		}
+
+		LOG_INF("Certificate mismatch");
+		err = modem_key_mgmt_delete(TLS_SEC_TAG, MODEM_KEY_MGMT_CRED_TYPE_CA_CHAIN);
+		if (err) {
+			LOG_ERR("Failed to delete existing certificate, err %d", err);
+		}
+	}
+
+	LOG_INF("Provisioning certificate to the modem");
+
+	/*  Provision certificate to the modem */
+	err = modem_key_mgmt_write(TLS_SEC_TAG, MODEM_KEY_MGMT_CRED_TYPE_CA_CHAIN, cert,
+				   sizeof(cert));
+	if (err) {
+		LOG_ERR("Failed to provision certificate, err %d", err);
+		return err;
+	}
+#else /* CONFIG_MODEM_KEY_MGMT */
+	#error not supported
+#endif /* !CONFIG_MODEM_KEY_MGMT */
+
+	return 0;
+}
+
+#include <modem/nrf_modem_lib.h>
+
+// Cert provisioning has to happen after modem lib initialization
+NRF_MODEM_LIB_ON_INIT(provision_certs, on_modem_lib_init, NULL);
+
+static void on_modem_lib_init(int ret, void *ctx) {
+	if (ret != 0) {
+		/* Return if modem initialization failed */
+		return;
+	}
+
+	int err = cert_provision();
+	if (err) {
+		LOG_ERR("Failed to provision certificates, error: %d", err);
+	}
+}
+
+#include <zephyr/net/socket.h>
+#include <zephyr/net/tls_credentials.h>
+
+#define CONFIG_HTTPS_HOSTNAME "ecdsa-test.memfault.com"
+#define HTTPS_PORT "443"
+
+/* Setup TLS options on a given socket */
+int tls_setup(int fd)
+{
+	int err;
+	int verify;
+
+	/* Security tag that we have provisioned the certificate with */
+	const sec_tag_t tls_sec_tag[] = {
+		TLS_SEC_TAG,
+	};
+
+	/* Set up TLS peer verification */
+	enum {
+		NONE = 0,
+		OPTIONAL = 1,
+		REQUIRED = 2,
+	};
+
+	verify = REQUIRED;
+
+	err = setsockopt(fd, SOL_TLS, TLS_PEER_VERIFY, &verify, sizeof(verify));
+	if (err) {
+		LOG_ERR("Failed to setup peer verification, err %d", errno);
+		return err;
+	}
+
+	/* Associate the socket with the security tag
+	 * we have provisioned the certificate with.
+	 */
+	err = setsockopt(fd, SOL_TLS, TLS_SEC_TAG_LIST, tls_sec_tag, sizeof(tls_sec_tag));
+	if (err) {
+		LOG_ERR("Failed to setup TLS sec tag, err %d", errno);
+		return err;
+	}
+
+	err = setsockopt(fd, SOL_TLS, TLS_HOSTNAME,
+			CONFIG_HTTPS_HOSTNAME,
+			sizeof(CONFIG_HTTPS_HOSTNAME) - 1);
+	if (err) {
+		LOG_ERR("Failed to setup TLS hostname, err %d", errno);
+		return err;
+	}
+	return 0;
+}
+
+static void send_http_request(void)
+{
+	int err;
+	int fd;
+	struct addrinfo *res;
+	struct addrinfo hints = {
+		.ai_flags = AI_NUMERICSERV, /* Let getaddrinfo() set port */
+		.ai_socktype = SOCK_STREAM,
+	};
+	char peer_addr[INET6_ADDRSTRLEN];
+
+	LOG_INF("🕵️ Looking up %s", CONFIG_HTTPS_HOSTNAME);
+
+	err = getaddrinfo(CONFIG_HTTPS_HOSTNAME, HTTPS_PORT, &hints, &res);
+	if (err) {
+		LOG_ERR("getaddrinfo() failed, err %d", errno);
+		return;
+	}
+
+	inet_ntop(res->ai_family, &((struct sockaddr_in *)(res->ai_addr))->sin_addr, peer_addr,
+		  INET6_ADDRSTRLEN);
+	LOG_INF("✅ Resolved %s (%s)", peer_addr, net_family2str(res->ai_family));
+
+	if (IS_ENABLED(CONFIG_SAMPLE_TFM_MBEDTLS)) {
+		fd = socket(res->ai_family, SOCK_STREAM | SOCK_NATIVE_TLS, IPPROTO_TLS_1_2);
+	} else {
+		fd = socket(res->ai_family, SOCK_STREAM, IPPROTO_TLS_1_2);
+	}
+	if (fd == -1) {
+		LOG_ERR("Failed to open socket!");
+		goto clean_up;
+	}
+
+	/* Setup TLS socket options */
+	err = tls_setup(fd);
+	if (err) {
+		goto clean_up;
+	}
+
+	LOG_INF("🔌 Connecting to %s:%d", CONFIG_HTTPS_HOSTNAME,
+	       ntohs(((struct sockaddr_in *)(res->ai_addr))->sin_port));
+	err = connect(fd, res->ai_addr, res->ai_addrlen);
+	if (err) {
+		LOG_ERR("connect() failed, err: %d", errno);
+		goto clean_up;
+	}
+
+	LOG_INF("✅ Connection succeeded");
+
+	clean_up:
+	freeaddrinfo(res);
+	(void)close(fd);
+}
+
+
 int main(void)
 {
 	int err;
+
+	// 2 second delay so logs aren't eaten
+	k_sleep(K_SECONDS(2));
 
 	LOG_INF("Memfault sample has started");
 
@@ -148,6 +382,7 @@ int main(void)
 	if (err) {
 		LOG_ERR("dk_buttons_init, error: %d", err);
 	}
+
 	/* Setup handler for Zephyr NET Connection Manager events. */
 	net_mgmt_init_event_callback(&l4_cb, l4_event_handler, L4_EVENT_MASK);
 	net_mgmt_add_event_callback(&l4_cb);
@@ -185,6 +420,7 @@ int main(void)
 	while (1) {
 		k_sem_take(&nw_connected_sem, K_FOREVER);
 		LOG_INF("Connected to network");
+		send_http_request();
 		on_connect();
 	}
 }
